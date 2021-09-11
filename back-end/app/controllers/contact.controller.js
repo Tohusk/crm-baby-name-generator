8ec833a47141babd0cdb15706b7be406ea9f0d02
Stella Li <stellayunzhili@gmail.com>
2021-09-11 14:59:20 +1000

    Resolved merge conflicts (main to backend contacts)

--- back-end/app/controllers/contact.controller.js
+++ back-end/app/controllers/contact.controller.js
@@ -10,88 +10,89 @@ const mongoose = require("mongoose");
  * Controller for initalise a user's contacts list
  */
 const initialiseContact = async (userId) => {
-    await Contacts.findOneAndDelete({user: userId})
+  await Contacts.findOneAndDelete({ user: userId });
 
-    // new contacts
-    const contacts = new Contacts ({
-        user: userId
-    })
+  // new contacts
+  const contacts = new Contacts({
+    user: userId,
+  });
 
-    // save contacts
-    await contacts.save();
-}
+  // save contacts
+  await contacts.save();
+};
 
 /**
  * Controller for saving a new contact
  */
 const newContact = async (req, res) => {
-    try {
-        // new contact model
-        const newContact = {
-            name: req.body.name,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            companyName: req.body.companyName,
-            description: req.body.description
-        }
+  try {
+    // new contact model
+    const newContact = {
+      name: req.body.name,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      companyName: req.body.companyName,
+      description: req.body.description,
+    };
 
-        // add a contact to a user's contact list
-        await Contacts.findOneAndUpdate(
-            {user: mongoose.Types.ObjectId(req.body.userId)},
-            {$push: {
-                    customers: newContact
-                }
-            });
-        res.send({ message: "New contact added successfully!" });
-    } catch (err){
-        console.log(err);
-        res.status(500).send({ message: err });
-        return;
-    }
-
-}
+    // add a contact to a user's contact list
+    await Contacts.findOneAndUpdate(
+      { user: mongoose.Types.ObjectId(req.body.userId) },
+      {
+        $push: {
+          customers: newContact,
+        },
+      }
+    );
+    res.send({ message: "New contact added successfully!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+    return;
+  }
+};
 
 /**
  * Controller to get one contact
  */
 const getContact = async (req, res) => {
-    try {
-        const contact = await Contacts.find({user: mongoose.Types.ObjectId(req.query.userId)})
-            .select({customers: {$elemMatch: {_id: mongoose.Types.ObjectId(req.query.contactId)}}});
-        res.json(contact);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({ message: err });
-        return;
-    }
-}
+  try {
+    const contact = await Contacts.find({ user: mongoose.Types.ObjectId(req.query.userId) }).select({
+      customers: { $elemMatch: { _id: mongoose.Types.ObjectId(req.query.contactId) } },
+    });
+    res.json(contact);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+    return;
+  }
+};
 
 /**
  * Controller to get a user's contact list
  */
 const getAllContacts = async (req, res) => {
-    try {
-        const contacts = await Contacts.find({user: mongoose.Types.ObjectId(req.query.userId)});
-        res.json(contacts);
-    } catch (err) {
-        res.status(500).send({ message: err });
-        return;
-    }
-}
+  try {
+    const contacts = await Contacts.find({ user: mongoose.Types.ObjectId(req.query.userId) });
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).send({ message: err });
+    return;
+  }
+};
 
 /**
  * Controller for deleting contacts entry for a given user
  */
 const deleteAllContacts = async (userId) => {
-    await Contacts.findOneAndDelete({user: mongoose.Types.ObjectId(userId)});
-}
+  await Contacts.findOneAndDelete({ user: mongoose.Types.ObjectId(userId) });
+};
 
 module.exports = {
-    initialiseContact,
-    newContact,
-    // updateContact,
-    getContact,
-    getAllContacts,
-    deleteAllContacts
-}
-
+  initialiseContact,
+  newContact,
+  // updateContact,
+  getContact,
+  getAllContacts,
+  deleteAllContacts,
+};
--- back-end/app/routes/contact.routes.js
+++ back-end/app/routes/contact.routes.js
@@ -2,7 +2,7 @@
  * routes relating to contacts
  */
 
-const { verifyContact} = require("../middlewares");
+const { verifyContact } = require("../middlewares");
 const controller = require("../controllers/contact.controller");
 
 /**
@@ -12,32 +12,23 @@ const controller = require("../controllers/contact.controller");
  * GET /api/contact/getAll
  * @param app
  */
-module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
+    next();
+  });
 
-    app.post(
-        "/api/contact/new",
-        [
-            verifyContact.checkRequiredFields
-        ],
-        controller.newContact
-    );
+  app.post("/api/contact/new", [verifyContact.checkRequiredFields], controller.newContact);
 
-    // app.post(
-    //     "/api/contact/update",
-    //     [
-    //         verifyContact.checkRequiredFields
-    //     ],
-    //     controller.updateContact
-    // );
+  // app.post(
+  //     "/api/contact/update",
+  //     [
+  //         verifyContact.checkRequiredFields
+  //     ],
+  //     controller.updateContact
+  // );
 
-    app.get("/api/contact/get", controller.getContact);
+  app.get("/api/contact/get", controller.getContact);
 
-    app.get("/api/contact/getAll", controller.getAllContacts);
+  app.get("/api/contact/getAll", controller.getAllContacts);
 };
