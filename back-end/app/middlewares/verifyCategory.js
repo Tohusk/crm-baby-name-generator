8b20fc3791502bb78cb6e53bcc9aafe7a0c49a0e
kcanares <canares.kyla@gmail.com>
2021-09-13 14:23:58 +1000

    created verifyCategory.js

---
+++ back-end/app/middlewares/verifyCategory.js
@@ -0,0 +1,67 @@
+/**
+ * middleware that helps check if a category is valid
+ */
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+/**
+ * check if a duplicate category for a user is trying to be created
+ */
+checkDuplicateUserCategory = (req, res, next) => {
+    // Username
+    User.findOne({
+        username: req.body.user
+    }).exec((err, user) => {
+        User.findOne({
+            email: req.body.name
+        }).exec((err, user) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+
+            if (user) {
+                res.status(400).send({ message: "Failed! Category already created!" });
+                return;
+            }
+
+            next();
+        });
+    });
+};
+
+/**
+ * check if all the required fields are filled
+ */
+const checkRequiredFields = (req, res, next) => {
+    if (!req.body.name) {
+        res.status(400).send({ message: "Failed! Need category name!" });
+        return;
+    }
+    if (!req.body.userId) {
+        res.status(400).send({ message: "Failed! Needs to provide userId!" });
+        return;
+    }
+
+    next();
+};
+
+const checkRequiredFieldsUpdate = (req, res, next) => {
+    if (!req.body.name) {
+        res.status(400).send({ message: "Failed! Need contact name!" });
+        return;
+    }
+    if (!req.body.userId || !req.body.categoryId) {
+        res.status(400).send({ message: "Failed! Needs to provide userId or categoryId!" });
+        return;
+    }
+
+    next();
+};
+
+const verifyContact = {
+    checkRequiredFields,
+    checkRequiredFieldsUpdate,
+};
+
+module.exports = verifyContact;
