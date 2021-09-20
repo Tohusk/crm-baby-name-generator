101ac728ec15e72b6e367d0af4f5064a4ce34cd1
kcanares <canares.kyla@gmail.com>
2021-09-20 11:04:38 +1000

    updated verfication index to include product

--- back-end/app/middlewares/index.js
+++ back-end/app/middlewares/index.js
@@ -1,9 +1,11 @@
 const authJwt = require("./authJwt");
 const verifySignUp = require("./verifySignUp");
 const verifyContact = require("./verifyContact");
+const verifyProduct = require("./verifyProduct");
 
 module.exports = {
     authJwt,
     verifySignUp,
     verifyContact,
+    verifyProduct,
 };
