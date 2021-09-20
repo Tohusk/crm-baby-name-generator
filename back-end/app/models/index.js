feef3b9c65fd2e47c52ac793aee5abe78bd6c2cb
kcanares <canares.kyla@gmail.com>
2021-09-20 11:03:30 +1000

    included product in index

--- back-end/app/models/index.js
+++ back-end/app/models/index.js
@@ -8,6 +8,7 @@ db.mongoose = mongoose;
 db.user = require("./user.model");
 db.role = require("./role.model");
 db.contacts = require("./contacts.model");
+db.product = require("./product.model");
 db.category = require("./category.model");
 
 db.ROLES = ["user", "admin"]; //, "moderator"];
