b6803006949977219ace029c4ccf868153ae5ccd
CaptainSams <33451949+Tohusk@users.noreply.github.com>
2021-10-17 00:06:51 +1100

    Added dynamic display of total customers and average satisfaction score

--- front-end/src/components/customer-overview.component.js
+++ front-end/src/components/customer-overview.component.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, isValidElement } from "react";
 import AuthService from "../services/auth.service";
 import { Link } from "react-router-dom";
 import CustomerList from "./customer-list.component";
+import ContactService from "../services/contact.service";
 import { Redirect } from "react-router";
+import { Bar } from 'react-chartjs-2';
 
 import "../styles/Home.css";
 import "../styles/Overview.css";
@@ -13,7 +15,44 @@ export default class Customers extends Component {
 
         this.state = {
             currentUser: AuthService.getCurrentUser(),
+            avgScore: 'N/A',
+            totalContacts: 0,
         };
+    }
+
+    async componentDidMount() {
+        try {
+            const allContacts = await ContactService.getAllCustomers(this.state.currentUser.id);
+            const numContacts = allContacts.data.length;
+            let total = 0;
+            let validScoreContacts = 0;
+            for (const contact of allContacts.data) {
+                const res = await ContactService.getContactStatistics(this.state.currentUser.id, contact._id);
+                if (res.data.averageRating !== 0) {
+                    total += res.data.averageRating;
+                    validScoreContacts += 1;
+                }
+            }
+            console.log(validScoreContacts);
+            if (validScoreContacts === 0) {
+                this.setState({
+                    avgScore: 'N/A',
+                    totalContacts: numContacts,
+                });
+            }
+            else {
+                const avgScore = total / validScoreContacts;
+                this.setState({
+                    avgScore: avgScore,
+                    totalContacts: numContacts,
+                });
+            }
+        } catch (err) {
+            this.setState({
+                avgScore: 'N/A',
+                totalContacts: 'N/A',
+            });
+        }
     }
 
     render() {
@@ -39,16 +78,19 @@ export default class Customers extends Component {
                 </div>
                 <div className="overview-subheading">Overview</div>
                 <div className="overview-flex-container">
-                    <div className="overview-stats-card">
-                        <div className="overview-card-heading">Total Customers</div>
-                        <div className="overview-card-stat">20</div>
+                    <div className="overview-stats">
+                        <div className="overview-stats-card">
+                            <div className="overview-card-heading">Total Customers</div>
+                            <div className="overview-card-stat">{this.state.totalContacts}</div>
+                        </div>
+                        <div className="overview-stats-card">
+                            <div className="overview-card-heading">Average Satisfaction Score</div>
+                            <div className="overview-card-stat">{this.state.avgScore}</div>
+                        </div>
                     </div>
-                    <div className="overview-stats-card">
-                        <div className="overview-card-heading">Satisfaction Score {">"} 4.0</div>
-                        <div className="overview-card-stat">51%</div>
-                    </div>
-                    <div className="overview-stats-card">
+                    <div className="overview-graph-card">
                         <div className="overview-card-heading">Number of Customers (graph)</div>
+
                     </div>
                 </div>
                 <div className="overview-subheading">Customer List</div>
