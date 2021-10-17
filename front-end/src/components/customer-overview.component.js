688756863c0ac700e18627e2026a9311d075c07d
CaptainSams <33451949+Tohusk@users.noreply.github.com>
2021-10-17 22:43:24 +1100

    Added line chart

--- front-end/src/components/customer-overview.component.js
+++ front-end/src/components/customer-overview.component.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import CustomerList from "./customer-list.component";
 import ContactService from "../services/contact.service";
 import { Redirect } from "react-router";
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 import "../styles/Home.css";
 import "../styles/Overview.css";
@@ -17,7 +17,40 @@ export default class Customers extends Component {
             currentUser: AuthService.getCurrentUser(),
             avgScore: 'N/A',
             totalContacts: 0,
+            labels: [],
+            datasets: [],
         };
+    }
+
+    getGraphData(allContacts) {
+        
+        // Loop through all contacts and find dates to make labels,
+        // number of customers on those dates is data
+        const customerDatesHashMap = {};
+        for (const contact of allContacts) {
+            if (contact.dateAdded !== undefined) {
+                customerDatesHashMap[contact.dateAdded.substring(0,10)] = (customerDatesHashMap[contact.dateAdded.substring(0,10)] || 0) + 1;
+            }
+        }
+
+        let labels = [];
+        let data = [];
+        for (const entry in customerDatesHashMap) {
+            labels.push(entry);
+            data.push(customerDatesHashMap[entry]);
+        }
+
+        const datasets = [{
+            data: data,
+            fill: true,
+            borderColor: 'rgb(82, 137, 223)',
+            backgroundColor: 'rgba(165, 222, 255, 0.4)'
+        }]
+
+        this.setState({
+            labels: labels,
+            datasets: datasets,
+        })
     }
 
     async componentDidMount() {
@@ -26,14 +59,11 @@ export default class Customers extends Component {
             const allContacts = await ContactService.getAllCustomers(this.state.currentUser.id);
             const numContacts = allContacts.data.length;
 
+            this.getGraphData(allContacts.data);
+
             if (res.data.avgUserRating !== null) {
                 this.setState({
                     avgScore: res.data.avgUserRating,
-                    totalContacts: numContacts,
-                });
-            }
-            else {
-                this.setState({
                     totalContacts: numContacts,
                 });
             }
@@ -42,6 +72,34 @@ export default class Customers extends Component {
                 avgScore: 'N/A',
                 totalContacts: 'N/A',
             });
+        }
+    }
+
+    showCustomerChart() {
+        if (this.state.labels.length === 0) {
+            return (<div className="overview-card-stat">No Data</div>);
+        } else {
+            return (<Line 
+                data={{
+                    labels: this.state.labels,
+                    datasets: this.state.datasets,
+                }} 
+                height={100}
+                options={{
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                    },
+                    scales: {
+                        yAxes: {
+                            ticks: {
+                                stepSize: 1,
+                            }
+                        }
+                    }
+                }}
+            />);
         }
     }
 
@@ -79,8 +137,8 @@ export default class Customers extends Component {
                         </div>
                     </div>
                     <div className="overview-graph-card">
-                        <div className="overview-card-heading">Number of Customers (graph)</div>
-
+                        <div className="overview-card-heading">Number of Customers</div>
+                        {this.showCustomerChart()}
                     </div>
                 </div>
                 <div className="overview-subheading">Customer List</div>
